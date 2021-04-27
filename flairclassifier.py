2c4e6ab4021ee36209bb5b599d2e222f952a95be
mirco gnuva <mirco.gnuva@studenti.unimi.it>
2021-04-27 18:05:12 +0200

    Refactored changing paradigm to OOP.

--- flairclassifier.py
+++ flairclassifier.py
@@ -7,6 +7,8 @@ import re
 import sqlite3
 import gc
 from tqdm import tqdm
+import os
+import argparse
 
 
 class Counter:
@@ -61,7 +63,7 @@ class Text:
 	def splittable(self) -> bool:
 		return self.text is not None and self.text != ''
 
-	def is_useful(self) -> bool:
+	def useful(self) -> bool:
 		return self.splittable() and len(self.sentences) > 0
 
 	def text_classification(self):
@@ -88,9 +90,10 @@ class Text:
 
 
 class Updater(Thread):
-	def __init__(self, queue_size: int):
+	def __init__(self, queue_size: int, db_path: str):
 		super().__init__()
 		self.params = []
+		self.path = db_path
 		self.queue_size = queue_size
 		self.query = f'UPDATE {table} set label = ?, conf = ? WHERE ROWID = ?;'
 
@@ -101,85 +104,162 @@ class Updater(Thread):
 		self.params.extend(params)
 
 	def run(self) -> None:
-		with sqlite3.connect(f'{root}{db}', check_same_thread=False) as write_conn:
+		with sqlite3.connect(self.path, check_same_thread=False) as write_conn:
 			write_conn.executemany(self.query, self.params)
 			write_conn.commit()
+			query = 'PRAGMA wal_checkpoint;'
+			write_conn.execute(query)
 
 
-BATCH_SIZE = 1000
-ROWS_BATCH = 100000
-table = 'tweet'
+class DBClassifier:
+	def __init__(self, path: str, table: str, mini_batch_size: int, classifier_batch_size: int, db_batch_size: int, classifier: str):
+		self.path = path
+		self.table = table
+		self.mini_batch_size = mini_batch_size
+		self.classifier_batch_size = classifier_batch_size
+		self.db_batch_size = db_batch_size
+		self.queue = Updater(self.db_batch_size, self.path)
+		self.old_queue = None
+		self.bar = tqdm(total=self.count_rows())
+		self.text_handlers = []
+		self.batch = []
+		self.text_classifier = TextClassifier.load(classifier)
+		self.read_conn = self.new_connection()
+		print(f'Path: {path} | Table: {table} | mini_batch: {mini_batch_size} '
+			  f'| classifier_batch: {classifier_batch_size} | db_batch: {db_batch_size} | classifier: {classifier}')
 
-root = "/mnt/hgfs/VMs_Shared/datasets/filtered/twitter/"
-db = 'merging_db.sqlite'
+	def reset_buffers(self):
+		self.text_handlers = []
+		self.batch = []
 
-conn = sqlite3.connect(f'{root}{db}', check_same_thread=False)
+	def new_connection(self) -> sqlite3.Connection:
+		conn = sqlite3.connect(self.path)
+		return conn
 
-conn.execute("PRAGMA journal_mode=WAL;")
-rows_count = conn.execute(f'SELECT COUNT(id) FROM tweet WHERE conf IS NULL;').fetchone()[0]
-query = 'SELECT ROWID, text, conf FROM tweet WHERE conf IS NULL;'
-cursor = conn.execute(query)
+	def get_rows_cursor(self) -> sqlite3.Cursor:
+		query = f'SELECT ROWID, text, conf FROM {self.table} WHERE conf IS NULL;'
+		cursor = self.read_conn.execute(query)
+		return cursor
 
-text_classifier = TextClassifier.load('sentiment-fast')
+	def count_rows(self) -> int:
+		query = f'SELECT COUNT(ROWID) FROM {self.table} WHERE conf IS NULL;'
+		with self.new_connection() as conn:
+			cursor = conn.execute(query)
+			rows_count = cursor.fetchone()[0]
+		return rows_count
 
-threads = []
-batch = []
-text_handlers = []
-queue = Updater(ROWS_BATCH)
+	def old_queue_running(self) -> bool:
+		return self.old_queue is not None and self.old_queue.is_alive()
 
-with tqdm(total=rows_count, desc='Reading rows') as bar:
-	for row in cursor:
-		rowid = row[0]
-		text = row[1]
-		if not text or text.strip() == '' or len(text.split()) < 2:
-			continue
-		text = text.strip()
-		text_handler = Text(rowid, text)
-		if not text_handler.is_useful():
-			with sqlite3.connect(f'{root}{db}', check_same_thread=False) as write_conn:
-				query = f'UPDATE {table} set label = "NEUTRAL", conf = 0 WHERE ROWID = {text_handler.rowid};'
-				write_conn.execute(query)
-				write_conn.commit()
+	def wait_old_queue(self):
+		if self.old_queue_running():
+			self.old_queue.join()
+			self.old_queue = None
+
+	def start_queue(self):
+		self.old_queue = self.queue
+		self.old_queue.start()
+		self.queue = Updater(self.db_batch_size, self.path)
+
+	def mark_useless_row(self, text_handler: Text):
+		self.start_queue()
+		self.wait_old_queue()
+		with sqlite3.connect(self.path) as write_conn:
+			query = f'UPDATE {self.table} set label = "NEUTRAL", conf = 0 WHERE ROWID = {text_handler.rowid};'
+			write_conn.execute(query)
+
+	def reset_queues(self):
+		self.queue = Updater(self.db_batch_size, self.path)
+		self.old_queue = None
+
+	def set_bar_desc(self, new_desc: str):
+		self.bar.desc = new_desc
+		self.bar.refresh()
+
+	def batch_ready(self) -> bool:
+		return len(self.batch) > self.classifier_batch_size
+
+	def classify_batch(self):
+		self.text_classifier.predict(self.batch, mini_batch_size=self.mini_batch_size)
+		params = [
+			(f'"{text_handler.text_classification()[0]}"', text_handler.text_classification()[1], text_handler.rowid)
+			for text_handler in self.text_handlers]
+		self.queue.add_params(params)
+
+	def process_batch(self):
+		self.set_bar_desc('Classifying')
+		self.classify_batch()
+		self.reset_buffers()
+		self.set_bar_desc('Reading db')
+
+	def close_queues(self):
+		self.set_bar_desc('Updating db')
+		self.wait_old_queue()
+		self.queue.start()
+		self.queue.join()
+
+	def classify_db(self):
+		self.set_bar_desc('Reading db')
+		rows_cursor = self.get_rows_cursor()
+		for row in rows_cursor:
+			rowid, text = row[:2]
+
+			if not text:
 				continue
 
-		text_handlers.append(text_handler)
-		batch.extend(text_handler.sentences)
+			text_handler = Text(rowid, text)
 
-		if len(batch) > BATCH_SIZE:
-			try:
-				bar.desc = 'Predicting'
-				bar.refresh()
-				text_classifier.predict(batch, mini_batch_size=12)
-				params = [(f'"{text_handler.text_classification()[0]}"', text_handler.text_classification()[1], text_handler.rowid) for text_handler in text_handlers]
-				queue.add_params(params)
-				if queue.full():
-					bar.desc = 'Updating database'
-					bar.refresh()
-					queue.start()
-					queue.join()
-					queue = Updater(ROWS_BATCH)
-				batch = []
-				text_handlers = []
-				gc.collect()
-				bar.desc = 'Reading rows'
-				bar.refresh()
-			except KeyboardInterrupt:
-				write_conn.commit()
-				write_conn.close()
-				exit(0)
-		bar.update()
+			if not text_handler.useful():
+				self.mark_useless_row(text_handler)
+				continue
 
-conn.close()
+			self.text_handlers.append(text_handler)
+			self.batch.extend(text_handler.sentences)
 
-try:
-	text_classifier.predict(batch)
-	queue = Updater(ROWS_BATCH)
-	params = [(f'"{text_handler.text_classification()[0]}"', text_handler.text_classification()[1], text_handler.rowid) for text_handler in text_handlers]
-	queue.add_params(params)
-	queue.start()
-	queue.join()
-	gc.collect()
-except KeyboardInterrupt:
-	write_conn.commit()
-	write_conn.close()
-	exit(0)
+			if self.batch_ready():
+				self.process_batch()
+
+			if self.queue.full():
+				self.set_bar_desc('Updating db')
+				self.wait_old_queue()
+				self.start_queue()
+				self.reset_queues()
+				self.set_bar_desc('Reading db')
+
+			self.bar.update()
+
+		self.read_conn.close()
+		self.process_batch()
+		self.close_queues()
+
+
+def init_argparser() -> argparse.ArgumentParser:
+	parser = argparse.ArgumentParser(
+		usage='Flair Classifier',
+		description="Classify table's text column to predict its label and confidence"
+	)
+	parser.add_argument('-p', '--path', help='path to db', required=True)
+	parser.add_argument('-t', '--table', help='table to classify', required=True)
+	parser.add_argument('-mb', '--mini-batch-size', type=int)
+	parser.add_argument('-cb', '--classifier-batch-size', type=int)
+	parser.add_argument('-db', '--db-batch-size', type=int)
+
+	return parser
+
+
+args_parser = init_argparser()
+args = args_parser.parse_args()
+
+
+MINI_BATCH_SIZE = args.mini_batch_size if args.mini_batch_size is not None else 500
+CLASSIFIER_BATCH_SIZE = args.classifier_batch_size if args.classifier_batch_size is not None else 1000
+DB_BATCH_SIZE = args.db_batch_size if args.db_batch_size is not None else 100000
+
+path = args.path
+table = args.table
+
+
+db_classifier = DBClassifier(path, table, MINI_BATCH_SIZE,
+							 CLASSIFIER_BATCH_SIZE, DB_BATCH_SIZE, 'sentiment-fast')
+
+db_classifier.classify_db()
