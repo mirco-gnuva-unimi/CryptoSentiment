cffd3f9a19acc44934c7f0a9177ad84ede0dbf5e
mirco.gnuva@studenti.unimi.it <zAcdGy3ThFWQDN6>
2021-05-16 00:55:17 +0200

    Palette wrapper to import it in all project.

---
+++ palette.py
@@ -0,0 +1,9 @@
+palette = {'positive': '#54a24b', 'smooth_positive': '#88d27a',
+		   'negative': '#e45756', 'smooth_positive': '#ff9d98',
+		   'price': '#4c78a8', 'smooth_price': '#9ecae9', 'strong_price': 'blue',
+		   'neutral': '#79706e', 'smooth_neutral': '#bab0ac',
+		   'neutral_1': '#ffd445', 'smooth_neutral_1': '#ffd95c', 'strong_neutral_1': '#f58518',
+		   'news': '#f58518', 'smooth_news': '#ffbf79',
+		   'reddit': '#439894', 'smooth_reddit': '#83bcb6',
+		   'twitter': '#F28482', 'smooth_twitter': '#E58986',
+		   'telegram': '#92D5E6', 'smooth_telegram': '#D3F6DB'}
