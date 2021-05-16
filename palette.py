10570fa36398cbd4bbf3d0429e1e9f78a0101542
mirco.gnuva@studenti.unimi.it <zAcdGy3ThFWQDN6>
2021-05-16 11:39:45 +0200

    Added 'strong_twitter' color

--- palette.py
+++ palette.py
@@ -5,5 +5,5 @@ palette = {'positive': '#54a24b', 'smooth_positive': '#88d27a',
 		   'neutral_1': '#ffd445', 'smooth_neutral_1': '#ffd95c', 'strong_neutral_1': '#f58518',
 		   'news': '#f58518', 'smooth_news': '#ffbf79',
 		   'reddit': '#439894', 'smooth_reddit': '#83bcb6',
-		   'twitter': '#F28482', 'smooth_twitter': '#E58986',
+		   'twitter': '#F28482', 'smooth_twitter': '#E58986', 'strong_twitter': '#995a59',
 		   'telegram': '#92D5E6', 'smooth_telegram': '#D3F6DB'}
