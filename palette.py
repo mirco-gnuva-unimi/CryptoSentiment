29d871b71cfc85c23c821df57b31bf062a3cb384
mirco.gnuva@studenti.unimi.it <zAcdGy3ThFWQDN6>
2021-05-16 14:33:04 +0200

    Added 'strong_telegram' color.

--- palette.py
+++ palette.py
@@ -6,4 +6,4 @@ palette = {'positive': '#54a24b', 'smooth_positive': '#88d27a',
 		   'news': '#f58518', 'smooth_news': '#ffbf79',
 		   'reddit': '#439894', 'smooth_reddit': '#83bcb6',
 		   'twitter': '#F28482', 'smooth_twitter': '#E58986', 'strong_twitter': '#995a59',
-		   'telegram': '#92D5E6', 'smooth_telegram': '#D3F6DB'}
+		   'telegram': '#92D5E6', 'smooth_telegram': '#D3F6DB', 'strong_telegram': '#6599a6'}
